6b417b7f38f1ed471c72bc7cfebcb89810f3dbb4
hirvaDhandukia <hirva.dhandhukia@gmail.com>
2024-10-23 16:28:00 -0300

    feat: class dependency graph demo data
    shows visual representation of internal dependencies in graph format
    using demo data

--- front-end/src/components/graphWhiteBoard/GraphWhiteBoard.tsx
+++ front-end/src/components/graphWhiteBoard/GraphWhiteBoard.tsx
@@ -1,7 +1,28 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import { ChangeEventHandler, useCallback, useState, useEffect } from 'react';
+import { ReactFlow, 
+    useNodesState,
+    useEdgesState,
+    addEdge, 
+    Connection
+  } from "@xyflow/react";
+
+import '@xyflow/react/dist/style.css';
+import { Container, Box } from '@mui/material';
+
+import { initialNodes } from './Nodes';
+import { initialEdges } from './Edges';
 
 function GraphWhiteBoard() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
   return (
     <div>
       {/* graph goes here */}
@@ -9,7 +30,25 @@ function GraphWhiteBoard() {
             Dependency Graph 
           </Typography>
           <Typography variant="body1" paragraph>
-            Content goes here for the right part.
+            <Container fixed>
+            <Box sx={{ 
+                    bgcolor: '#ebedef', 
+                    height: '70vh',
+                    padding: '10px', 
+                    marginTop: '50px'
+                }}>
+                <ReactFlow 
+                    nodes={nodes} 
+                    edges={edges} 
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    fitView
+                >
+                </ReactFlow>
+            </Box>
+            
+        </Container>
           </Typography>
     </div>
   )
