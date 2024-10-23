f632590fbc9aa3dcf9e90dac1e5e0e845a764239
hirvaDhandukia <hirva.dhandhukia@gmail.com>
2024-10-23 17:13:19 -0300

    fix: position of nodes in graph
    node and edge are aligned automatically as per connection between classes

--- front-end/src/components/graphWhiteBoard/GraphWhiteBoard.tsx
+++ front-end/src/components/graphWhiteBoard/GraphWhiteBoard.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import dagre from 'dagre';
 import { ChangeEventHandler, useCallback, useState, useEffect } from 'react';
 import { ReactFlow, 
     useNodesState,
@@ -14,6 +15,42 @@ import { Container, Box } from '@mui/material';
 import { initialNodes } from './Nodes';
 import { initialEdges } from './Edges';
 
+// implementaiton of dagre in the graph 
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+// layouting nodes and edges as per ranking
+const getLayoutedElements = (nodes: any, edges: any) => {
+  const isHorizontal = false;
+  dagreGraph.setGraph({ rankdir: isHorizontal ? 'LR' : 'TB' });
+
+  // setNodes parsing in dagre
+  nodes.forEach((node: any) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  // setEdge parsing in dagre
+  edges.forEach((edge: any) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node: any) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+    return node;
+  });
+
+  return { nodes, edges };
+};
+
 function GraphWhiteBoard() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
@@ -22,6 +59,13 @@ function GraphWhiteBoard() {
     (params: Connection) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
   );
+
+  // running dagre in nodes
+  useEffect(() => {
+    const layoutedElements = getLayoutedElements(nodes, edges);
+    setNodes([...layoutedElements.nodes]);
+    setEdges([...layoutedElements.edges]);
+  }, []);
 
   return (
     <div>
