6720d0314f4d9f16b5edcd65cdef12cece932c3b
Viren Joshi <viren.joshi403@gmail.com>
2024-11-28 20:04:52 -0400

    chore: deployment changes

--- Back-end/src/main/java/com/g8/configuration/FirebaseConfig.java
+++ Back-end/src/main/java/com/g8/configuration/FirebaseConfig.java
@@ -7,16 +7,18 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.FirestoreClient;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
+// Keeping these imports for running the project locally.
+import io.github.cdimascio.dotenv.Dotenv;
+import java.io.File;
+import java.io.FileInputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -27,11 +29,16 @@ public class FirebaseConfig {
     public FirebaseAuth firebaseAuth() {
 
         try {
-            Dotenv dotenv = Dotenv.load();
-            String credentialsPath = dotenv.get("GOOGLE_APPLICATION_CREDENTIALS");
-            FileInputStream serviceAccount = new FileInputStream(System.getProperty("user.dir") + File.separator + credentialsPath);
-
-            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+            /*
+             * The code commented below is for running the project locally. It reads the [.env] file to get the path to the Firebase credentials file.
+             * and the [.env] file should be in the root directory of the project. It should contain the variable
+             * GOOGLE_APPLICATION_CREDENTIALS=path/to/firebase-config.json
+             */
+            // Dotenv dotenv = Dotenv.load();
+            // String credentialsPath = dotenv.get("GOOGLE_APPLICATION_CREDENTIALS");
+            // FileInputStream serviceAccount = new FileInputStream(System.getProperty("user.dir") + File.separator + credentialsPath);
+            // FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.getApplicationDefault()).build(); // Comment this out, if you are using the commented code above.
             FirebaseApp.initializeApp(options);
             return FirebaseAuth.getInstance();
 
--- Back-end/src/main/java/com/g8/controller/AuthController.java
+++ Back-end/src/main/java/com/g8/controller/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
 public class AuthController {
     
     private final AuthService authService;
